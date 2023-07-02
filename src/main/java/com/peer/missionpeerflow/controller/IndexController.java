3f75b670f7a4cd2c81c796ea7702fb0f1afd39f4
yonghlee <cjswl1357@gmail.com>
2023-07-02 13:56:57 +0900

    feat: 서버배포

--- src/main/java/com/peer/missionpeerflow/controller/IndexController.java
+++ src/main/java/com/peer/missionpeerflow/controller/IndexController.java
@@ -21,50 +21,6 @@ public class IndexController {
     private final IndexService indexService;
 
     private final QuestionRepository questionRepository;
-    @GetMapping("/test1")
-    public void test()
-    {
-            Question question = new Question();
-            question.setTitle("안녕하세요");
-            question.setView(1L);
-            question.setCategory(Category.MINISHELL);
-            question.setRecommend(1L);
-            question.setContent("메우 안녕하세요");
-            question.setCreatedAt(LocalDateTime.now());
-            question.setNickname("용훈");
-            question.setPassword("1234");
-            questionRepository.save(question);
-    }
-
-    @GetMapping("/test2")
-    public void test2()
-    {
-        Question question = new Question();
-        question.setTitle("안녕하세요222");
-        question.setView(1L);
-        question.setCategory(Category.FT_IRC);
-        question.setRecommend(1L);
-        question.setContent("메우 안녕하세요222");
-        question.setCreatedAt(LocalDateTime.now());
-        question.setNickname("형찬");
-        question.setPassword("1234");
-        questionRepository.save(question);
-    }
-
-    @GetMapping("/test3")
-    public void test3()
-    {
-        Question question = new Question();
-        question.setTitle("안녕하세요33333");
-        question.setView(1L);
-        question.setCategory(Category.MINIRT);
-        question.setRecommend(1L);
-        question.setContent("메우 안녕하세요3333333");
-        question.setCreatedAt(LocalDateTime.now());
-        question.setNickname("소현");
-        question.setPassword("1234");
-        questionRepository.save(question);
-    }
 
     @GetMapping("")
     public ResponseEntity<Page<IndexDTO>> getIndex(
